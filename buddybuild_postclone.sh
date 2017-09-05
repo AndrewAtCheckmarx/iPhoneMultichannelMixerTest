35e930326edb797048570270dda9ffd63e320794
AndrewAtCheckmarx <andrew.thompson@checkmarx.com>
2017-09-05 17:51:05 +0100

    Update buddybuild_postclone.sh

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-.\CxConsole\runBNCxScript.sh -p "All" -n "BB Test" -t "CxServer\\SP\\EMEA\\Checkmarx.com-UK" -h 5 -m 6
+./CxConsole/runBNCxScript.sh -p "All" -n "BB Test" -t "CxServer\\SP\\EMEA\\Checkmarx.com-UK" -h 5 -m 6
